5df278cbde55785f73ff57c3ffe85e401a938327
Utkarsh Singh <utkarshsingh369@gmail.com>
2020-07-24 00:40:09 +0530

    Update bookings.js

--- node-restful-api/api/routes/bookings.js
+++ node-restful-api/api/routes/bookings.js
@@ -11,6 +11,11 @@ router.post("/", checkAuth, BookingsController.confirm_booking);
 
 router.get("/:bookingId", checkAuth, BookingsController.get_booking);
 
+router.get('/checkout', async (req, res) => {
+  const session = // ... Fetch or create the Checkout Session
+  res.render('checkout', { session_id: session.id });
+});
+
 router.delete("/:bookingId", checkAuth, BookingsController.delete_booking);
 
 module.exports = router;
