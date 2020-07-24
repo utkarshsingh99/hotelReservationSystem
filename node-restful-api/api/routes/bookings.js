857058ce2e04629fbff0191caf9e806d0180e426
utkarshsingh99 <utkarshsingh369@gmail.com>
2020-07-24 11:42:37 +0530

    checkout update - I

--- node-restful-api/api/routes/bookings.js
+++ node-restful-api/api/routes/bookings.js
@@ -12,8 +12,26 @@ router.post("/", checkAuth, BookingsController.confirm_booking);
 router.get("/:bookingId", checkAuth, BookingsController.get_booking);
 
 router.get('/checkout', async (req, res) => {
-    const session = // ... Fetch or create the Checkout Session
-    res.render('checkout', session_id: <%=session.id %>);
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "T-shirt",
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url:
+        "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: "https://example.com/cancel",
+    });
+    res.render('checkout', {session_id: session.id });
   });
 
 router.delete("/:bookingId", checkAuth, BookingsController.delete_booking);
